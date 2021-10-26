34b139dd0c3a778b2fb42719c5616848e3aed3bf
Erin <etruman2@gmail.com>
2021-10-26 15:01:56 -0500

    added category to models and embedded users and categories in the get posts function

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 class User():
-    def __init__(self, id, first_name, last_name, email, bio, username, password, created_on, active):
+    def __init__(self, id, first_name, last_name, email, bio, username, created_on, active, password=""):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.bio = bio
         self.username = username
-        self.password = password
         self.created_on = created_on
         self.active = active
+        self.password = password
+        
